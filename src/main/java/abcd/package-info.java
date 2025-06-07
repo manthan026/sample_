fc6c2d3a214f7f3f093466762cf7dd4ef25c1e96
manthan026 <manthansuryavanshi55@gmail.com>
2025-06-07 16:23:17 +0530

    first commit

---
+++ src/main/java/abcd/package-info.java
@@ -0,0 +1,20 @@
+package abcd;
+
+class operation{
+	
+	public static void addition() {
+		
+	}
+	
+	public static void substraction() {
+		
+	}
+
+	public static void multiplicationtion() {
+	
+	}
+
+	public static void division() {
+	
+	}
+}
