b30267cd48b55ddb8da7ed455e12b3b8a7ccb876
Shripad21 <shripadraccha143@gmail.com>
2025-06-07 17:20:34 +0530

    addition not done

--- src/main/java/abcd/operation.java
+++ src/main/java/abcd/operation.java
@@ -1,8 +1,15 @@
 package abcd;
 
 class operation{
-	
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
 	public static void addition() {
+		
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		System.out.println("Addition is:- " + c);
 		
 	}
 	
